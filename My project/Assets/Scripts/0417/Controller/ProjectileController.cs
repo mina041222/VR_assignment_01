8b82d87d73ae7221e2b1a568d9d3da99573837e3
mina041222 <minajeon2004@gmail.com>
2023-05-31 17:32:33 +0900

    씬 복구

--- My project/Assets/Scripts/0417/Controller/ProjectileController.cs
+++ My project/Assets/Scripts/0417/Controller/ProjectileController.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class ProjectileController : MonoBehaviour
 {
-    public GameObject Projectile;
+    public GameObject Projectile;       //발사체 프리팹 선언
 
+    //발사체를 생성 후 이동 백터를 넣어줘서 발사이동후 10초후 소멸
     public void FireProjectile()
     {
-        GameObject temp = Instantiate(Projectile);
+        //Instantiate 함수는 오브젝트 및 프리팹 생성하는 함수 
+        GameObject temp = (GameObject)Instantiate(Projectile);
+        //생성된 Projectile을 temp에 입력 
 
-        temp.transform.position = gameObject.transform.position;
-        temp.GetComponent<ProjectileMove>().launchDir = transform.forward;
-        Destroy(temp, 10f);
+        //해당 게임오브젝트 위치에서 생성
+        temp.transform.position = this.gameObject.transform.position;
+
+        //발사체에 발사 방향을 내 오브젝트의 앞쪽으로 설정한다. 
+        temp.GetComponent<ProjectileMove>().launchDirection = transform.forward;
+
+        //Destroy는 게임 오브젝트를 없에주는 함수 (10초후 소멸)
+        Destroy(temp, 10.0f);
     }
 }
--- My project/Assets/Scripts/0417/Controller/ProjectileMove.cs
+++ My project/Assets/Scripts/0417/Controller/ProjectileMove.cs
@@ -1,28 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+
 
 public class ProjectileMove : MonoBehaviour
 {
-    public Vector3 launchDir;
+    public enum PROJECTILETYPE              //�Ѿ� Ÿ�� ������ ���� Enum ����
+    {
+        PLAYER,
+        ENEMY
+    }
+
+    public Vector3 launchDirection;
+    public PROJECTILETYPE projectileType = PROJECTILETYPE.PLAYER;   //�Ѿ� Ÿ�� ����
+
+    private void OnCollisionEnter(Collision collision)
+    {   //���� �浹�� �ı�
+        if (collision.gameObject.tag == "Wall")
+        {
+            Destroy(this.gameObject);
+        }
+        //���Ϳ� �浹��
+        if (collision.gameObject.tag == "Monster")
+        {
+            //���Ϳ��� �������� �ְ� �������. 
+            collision.gameObject.GetComponent<MonsterController>().Damanged(1);
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)         //Trigger �Լ� 
+    {   //���� �浹�� �ı�
+        if (other.gameObject.tag == "Wall")             //Name -> Tag �� ��ȯ 
+        {
+            Destroy(this.gameObject);
+        }
+        //���Ϳ� �浹��
+        if (other.gameObject.tag == "Monster" && projectileType == PROJECTILETYPE.PLAYER)
+        {
+            //���Ϳ��� �������� �ְ� �������. 
+            other.gameObject.GetComponent<MonsterController>().Damanged(1);
+            other.transform.DOPunchScale(new Vector3(0.5f, 0.5f, 0.5f), 0.1f, 10, 1);
+            Destroy(this.gameObject);
+        }
+
+        if (other.gameObject.tag == "Player" && projectileType == PROJECTILETYPE.ENEMY)
+        {
+            //�÷��̾�� �������� �ְ� �������. 
+            other.gameObject.GetComponent<PlayerController>().Damanged(1);
+            Destroy(this.gameObject);
+        }
+    }
 
     private void FixedUpdate()
     {
-        float moveAmount = 3 * Time.deltaTime;
-        transform.Translate(launchDir * moveAmount);
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.name.Equals("Wall"))
-        {
-            Destroy(gameObject);
-        }
-
-        if (collision.gameObject.name.Equals("Monster"))
-        {
-            collision.gameObject.GetComponent<MonsterController>().Damanged(1);
-            Destroy(gameObject);
-        }
+        //�ð���� �̵� �� float ������ ����
+        float moveAmount = 3 * Time.fixedDeltaTime;
+        //launchDirection �������� �߻�ü �̵� (Translate) �̵� ��Ű�� �Լ�
+        transform.Translate(launchDirection * moveAmount);
     }
 }
